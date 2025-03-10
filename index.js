90ddc9c2c7881f61c463ac423935ac6017eb4676
OttoMachado <158228292+OttoMachado@users.noreply.github.com>
2025-03-10 05:26:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,111 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.iniciar = iniciar;
 var readlineSync = require("readline-sync");
+var Personagem_1 = require("./Personagem");
 var inimigo_1 = require("./inimigo");
 function iniciar() {
-    var nome = readlineSync.question("Qual seu nome Jovem viajante?");
-    console.log("Entendi seu nome viajante! ".concat(nome, " \u00E9 o nome de quem ir\u00E1 salvar a nossa vila desses monstros horr\u00EDveis"));
-    console.log("Ah... \u00E9 mesmo... parece que voc\u00EA n\u00E3o sabe nada do que est\u00E1 acontecendo aqui, jovem viajante.\n         Vou me esfor\u00E7ar para lembrar de tudo o que posso, embora o esquecimento tenha tomado conta do meu c\u00E9rebro e meus olhos n\u00E3o consigam mais enxergar nada. Por\u00E9m,\n          lembro que esta era uma vila bem tranquila at\u00E9 aquele maldito Urso aparecer e abrir um portal para a entrada de v\u00E1rias criaturas horrendas.\n           Hoje, precisamos nos esconder e tentar lutar contra esses monstros. Muitos dizem que, na verdade,\n         esse urso fez um pacto sat\u00E2nico para conseguir abrir um portal direto das profundezas do inferno. Nosso conhecimento sobre essa hist\u00F3ria \u00E9 muito rasa e ofuscado.");
-    console.log("".concat(nome, " eu sempre sonhei com o dia em que um jovem viajante viria de longe e salvaria a alma dessas pobre pessoas que andam t\u00E3o devastadas."));
-    console.log("eu estou velho demais pra isso, por favor salve a todos que residem nesse lugar.");
-    console.log("Siga em direção norte até chegar em uma caverna, tome muito cuidado e boa sorte.");
+    var personagemEscolhido = escolherClasse();
+    console.log("Bem vindo Jovem Viajante ".concat(personagemEscolhido.nome, " esse \u00E9 o inicio da sua aventura."));
+    console.log("Siga para o norte até encontrar uma caverna, tome muito cuidado e boa sorte.");
+    readlineSync.question("Aperte Enter pra continuar...");
+    if (lutaGoblin(personagemEscolhido)) {
+        var escolha = readlineSync.question("O que quer fazer agora?\n1. Explorar a passagem secreta.\n2. Procurar por itens.\nEscolha: ");
+    }
+    if (lutaBruxa(personagemEscolhido)) {
+        lutaUrsoECristiano(personagemEscolhido);
+    }
 }
-var continuarJogo = true;
-var goblin = new inimigo_1.Goblin("Goblin feio da p#rra", 1, 30, 20, 20, -20);
-while (continuarJogo) {
-    var acao = readlineSync.question("Voc\u00EA entrou na caverna e encontrou um Goblin feio da p#rra, O que voc\u00EA vai fazer agora?\n            \n            \n1 Op\u00E7\u00E3o 1: Atacar o Goblin feio da p#rra.\n\n            \n2 Op\u00E7\u00E3o 2: Mudar a rota seguindo pro lado Oposto do Goblin feio da p#rra.\n\n            \n3 Op\u00E7\u00E3o 3: Sair desse Jogo Ruim.\n\n            ");
-    switch (acao) {
-        case '1':
-            goblin.receberDano(10);
-            break;
-        case '2':
-            console.log("Voce decidiu fugir desse bicho feio do kct e saiu correndo pra outro lado da caverna");
-            break;
-        case '3':
-            console.log("Voce saiu do Jogo de tanto medo da cara desse Goblin.");
-            continuarJogo = false;
-            break;
+function escolherClasse() {
+    var nome = readlineSync.question('Qual seu nome, Jovem Viajante? ');
+    var escolha = readlineSync.question("Escolha a Classe: \n1. Guerreiro\n2. Mago\n3. Arqueiro\n4. Samurai\n5. Vampiro\nEscolha (1-5): ");
+    switch (escolha) {
+        case '1': return new Personagem_1.Guerreiro(nome);
+        case '2': return new Personagem_1.Mago(nome);
+        case '3': return new Personagem_1.Arqueiro(nome);
+        case '4': return new Personagem_1.Samurai(nome);
+        case '5': return new Personagem_1.Vampiro(nome);
         default:
-            console.log("Tá na Disney? Digita certo os números seu Animal.");
+            console.log('foi tanso e agora vai pegar um randola ai.');
+            return new Personagem_1.Guerreiro(nome);
+    }
+}
+function lutaGoblin(personagem) {
+    console.log("Você entrou na caverna e encontrou um Goblin feio da p#rra!");
+    var goblin = new inimigo_1.Goblin("Goblin", 1, 30, 20, 20, -20);
+    while (true) {
+        var acao = readlineSync.question("O que fazer?\n1. Atacar\n2. Fugir\n3. Sair do jogo\nEscolha: ");
+        switch (acao) {
+            case '1':
+                goblin.receberDano(10);
+                if (goblin.vida <= 0) {
+                    console.log("Você derrotou o Goblin feio da Desgraça");
+                    return true;
+                }
+                goblin.causarDano(personagem);
+                if (personagem.vida <= 0) {
+                    console.log("".concat(personagem.nome, " Morreu pro goblin feiokkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"));
+                    return false;
+                }
+                break;
+            case '2':
+                console.log("Você fugiu para outra parte da caverna.");
+                return true;
+            case '3':
+                console.log("Você saiu do jogo.");
+                return false;
+            default:
+                console.log("Digita um número válido animal.");
+        }
+    }
+}
+function lutaBruxa(personagem) {
+    console.log("Você segue pela passagem secreta e encontra uma velha desgraçada.");
+    var bruxa = new inimigo_1.Bruxa("Bruxa", 1, 40, 20, 20, -20);
+    while (true) {
+        var escolha = readlineSync.question("O que tu quer fazer? \n1. Atacar a Bruxa.\n2. Aceitar um feiti\u00E7o.\nEscolha: ");
+        switch (escolha) {
+            case '1':
+                bruxa.receberDano(10);
+                console.log("Você atacou a bruxa velha");
+                if (bruxa.vida <= 0) {
+                    console.log("Mato a véia na Paulada e Encontrou Dois caminhos.");
+                    return true;
+                }
+                bruxa.causarDano(personagem);
+                if (personagem.vida <= 0) {
+                    console.log("".concat(personagem.nome, " Morreu pra velha fudida!"));
+                    return false;
+                }
+                break;
+                break;
+            case '2':
+                console.log("A bruxa lançou um feitiço em você (é uma benção)");
+                return true;
+            default:
+                console.log("Opção inválida jegue.");
+        }
+    }
+}
+function lutaUrsoECristiano(personagem) {
+    console.log("Voce matou a bruxa e se Encontrou com o Urso de Metralhadora e o Cristiano Ronaldo de Bicleta.");
+    while (true) {
+        var escolha = readlineSync.question("O que vai escolher? \n1. Atacar eles.\n2. Fugir.\nEscolha: ");
+        switch (escolha) {
+            case '1':
+                if (escolha == '1') {
+                    console.log('Voce Morreu de forma Brutal (nunca brinque com um Urso de Metralhadora nem com o Cristiano Ronaldo de Bicleta!)');
+                    return true;
+                }
+                break;
+            case '2':
+                if (escolha == '2') {
+                    console.log("Voce Fugiu do mal e achou uma Forma de Salvar o Mundo Pacificamente! (Good Ending)");
+                    return true;
+                }
+                break;
+            default:
+                console.log("Opção inválida jegue.");
+        }
     }
 }
 iniciar();
